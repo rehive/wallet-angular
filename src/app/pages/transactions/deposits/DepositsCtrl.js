24058189f8cfe0825ca50eb0532d135dc41c5845
AquiburRKhan <ark.aquib17@gmail.com>
2017-03-27 19:19:36 +0600

    add: added confirm transaction panel,incomplete functionality

--- src/app/pages/transactions/deposits/DepositsCtrl.js
+++ src/app/pages/transactions/deposits/DepositsCtrl.js
@@ -5,17 +5,46 @@
         .controller('DepositsCtrl', DepositsCtrl);
 
     /** @ngInject */
-    function DepositsCtrl($rootScope,$scope,IMAGEURL) {
+    function DepositsCtrl($rootScope,$scope,IMAGEURL,$http,API,cookieManagement,toastr) {
 
-        $scope.showAdvancedOption = true;
+        var vm = this;
+        vm.token = cookieManagement.getCookie('TOKEN');
 
-        $scope.initialize = function () {
-            $scope.currencyImageUrl = IMAGEURL + $rootScope.selectedCurrency.code + '.png';
+        $scope.depositParams = {
+            user: '',
+            amount: '',
+            reference: '',
+            confirm_on_create: false
         };
+        $scope.showAdvancedOption = true;
+        $scope.confirmDeposit = false;
+        $scope.currencyImageUrl = IMAGEURL + $rootScope.selectedCurrency.code + '.png';
 
         $rootScope.$watch('selectedCurrency',function(){
             $scope.currencyImageUrl = IMAGEURL + $rootScope.selectedCurrency.code + '.png';
         });
+
+        $scope.toggleConfirmDeposit = function(confirmDeposit){
+            $scope.confirmDeposit = !confirmDeposit;
+        };
+
+        $scope.createDeposit = function () {
+            console.log($scope.depositParams);
+            $http.post(API + '/admin/transactions/deposit/', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'JWT ' + vm.token
+                },
+                params: $scope.depositParams
+            }).then(function (res) {
+                console.log(res);
+                if (res.status === 200) {
+                    toastr.success('You have successfully deposited your money!');
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+        }
 
 
     }
