cf1853b3b8c7f5ef08eba4a1d5b2865d04f62b85
AquiburRKhan <ark.aquib17@gmail.com>
2017-03-28 13:22:09 +0600

    change: changed transactions history file structure

---
+++ src/app/pages/transactions/history/filters/filters.directive.js
@@ -0,0 +1,16 @@
+
+(function () {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.transactions.history')
+        .directive('historyFilters', historyFilters);
+
+    /** @ngInject */
+    function historyFilters() {
+        return {
+            restrict: 'E',
+            controller: 'HistoryCtrl',
+            templateUrl: 'app/pages/transactions/history/filters/filters.html'
+        };
+    }
+})();
---
+++ src/app/pages/transactions/history/table/table.directive.js
@@ -0,0 +1,16 @@
+
+(function () {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.transactions.history')
+        .directive('historyTable', historyTable);
+
+    /** @ngInject */
+    function historyTable() {
+        return {
+            restrict: 'E',
+            controller: 'HistoryCtrl',
+            templateUrl: 'app/pages/transactions/history/table/table.html'
+        };
+    }
+})();
