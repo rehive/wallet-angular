f1b70ed73c257681a6529b195ab113bcc265e88c
Johan du Plessis <johandp92@gmail.com>
2017-03-28 15:13:40 +0200

    Fixed authentication error on post requests
    - Also just fixed indentation on login controller

--- src/app/pages/transactions/deposits/DepositsCtrl.js
+++ src/app/pages/transactions/deposits/DepositsCtrl.js
@@ -33,13 +33,13 @@
         };
 
         $scope.createDeposit = function () {
-            console.log($scope.depositParams);
-            $http.post(API + '/admin/transactions/deposit/', {
+            // $http.post takes the params as follow post(url, data, {config})
+            // https://docs.angularjs.org/api/ng/service/$http#post
+            $http.post(API + '/admin/transactions/deposit/', $scope.depositParams, {
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': 'JWT ' + vm.token
-                },
-                data: $scope.depositParams
+                }
             }).then(function (res) {
                 console.log(res);
                 if (res.status === 200) {
