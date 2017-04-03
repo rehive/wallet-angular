5ca11d1ae7806aec9efcdf4b9e2d6c3e2dd6b233
AquiburRKhan <ark.aquib17@gmail.com>
2017-04-03 19:42:31 +0600

    add: added tx_code,tx_type and status filters in transactions history table

--- src/app/pages/transactions/history/HistoryCtrl.js
+++ src/app/pages/transactions/history/HistoryCtrl.js
@@ -5,7 +5,7 @@
         .controller('HistoryCtrl', HistoryCtrl);
 
     /** @ngInject */
-    function HistoryCtrl($scope,API,$http,cookieManagement,$uibModal) {
+    function HistoryCtrl($rootScope,$scope,API,$http,cookieManagement,$uibModal) {
 
         var vm = this;
         vm.token = cookieManagement.getCookie('TOKEN');
@@ -18,20 +18,26 @@
             searchUser:'',
             date_from: '',
             date_to: '',
-            searchType: 'Type',
-            searchStatus: 'Status',
+            searchType: '',
+            searchStatus: '',
             searchCurrency: {},
             orderBy: 'Latest'
         };
 
-        $scope.transactions = [];
+        //used rootscope to communicate between directives
+        $rootScope.transactions = [];
+        $rootScope.transactionsStateMessage = '';
         $scope.loadingTransactions = false;
-        $scope.transactionsStateMessage = '';
-        $scope.typeOptions = ['Type','Deposit','Transfer','Withdraw'];
-        $scope.statusOptions = ['Status','Cancelled','Claimed','Complete','Denied','Expired','Failed','Incoming',
+        $scope.typeOptions = ['','deposit','transfer','withdraw'];
+        $scope.statusOptions = ['','Cancelled','Claimed','Complete','Denied','Expired','Failed','Incoming',
                                 'Incomplete','Pending','Processing','Reversed','Unclaimed','Uncredited','Waiting'];
         $scope.currencyOptions = [];
         $scope.orderByOptions = ['Largest','Latest','Smallest'];
+
+        $scope.$on('$locationChangeStart', function( event ) {
+            delete $rootScope.transactions;
+            delete $rootScope.transactionsStateMessage;
+        });
 
         vm.getCompanyCurrencies = function(){
             $http.get(API + '/company/currencies/', {
@@ -53,15 +59,20 @@
 
         $scope.getLatestTransactions = function(){
             $scope.loadingTransactions = true;
-            $scope.transactions = [];
+            if($rootScope.transactions.length > 0 ){
+                $rootScope.transactions.length = 0;
+            }
 
             vm.filterParams = '?page=' + $scope.page + '&page_size=' + vm.pageSize + '&orderby=' + vm.orderBy
-                + '&tx_code=' + $scope.searchParams.txCode; // all the params of the filtering
+                + '&tx_code=' + $scope.searchParams.txCode + '&tx_type=' + $scope.searchParams.searchType
+                + '&status=' + $scope.searchParams.searchStatus; // all the params of the filtering
 
             //console.log($scope.searchParams.searchUser);
-            //console.log(API + '/admin/transactions/' + vm.filterParams);
+            console.log(API + '/admin/transactions/' + vm.filterParams);
 
-            $http.get(API + '/admin/transactions/' + vm.filterParams, {
+            var transactionsUrl = API + '/admin/transactions/' + vm.filterParams;
+
+            $http.get(transactionsUrl, {
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': 'JWT ' + vm.token
@@ -69,16 +80,19 @@
             }).then(function (res) {
                 $scope.loadingTransactions = false;
                 if (res.status === 200) {
-                    if(res.data.data.results.length == 0){
-                        $scope.transactionsStateMessage = 'No Transactions Have Been Made';
+                    console.log(res.data.data);
+                    $rootScope.transactions = res.data.data.results;
+                    if($rootScope.transactions == 0){
+                        $rootScope.transactionsStateMessage = 'No Transactions Have Been Found';
+                        return;
                     }
-                    $scope.transactions = res.data.data.results;
-                    $scope.transactionsStateMessage = '';
-                    //console.log($scope.transactions);
+
+                    $rootScope.transactionsStateMessage = '';
+
                 }
             }).catch(function (error) {
                 $scope.loadingTransactions = false;
-                $scope.transactionsStateMessage = 'Failed To Load Data';
+                $rootScope.transactionsStateMessage = 'Failed To Load Data';
                 console.log(error);
             });
         };
