2bc8527d5aac3c8cfaa4a99cd1f067843c941c0c
AquiburRKhan <ark.aquib17@gmail.com>
2017-04-26 14:39:03 +0600

    add: added admin account info api and can update admin user info in settings

--- src/app/pages/settings/accountInfo/AccountInfoCtrl.js
+++ src/app/pages/settings/accountInfo/AccountInfoCtrl.js
@@ -10,8 +10,50 @@
         var vm = this;
         vm.token = cookieManagement.getCookie('TOKEN');
         $scope.companyImageUrl = "https://storage.googleapis.com/rehive-static/dashboard/dist/img/default_company_icon.png";
+        $scope.loadingAccountInfo = true;
+        vm.updatedAdministrator = {};
 
+        $scope.accountInfoChanged = function(field){
+            vm.updatedAdministrator[field] = $scope.administrator[field];
+        };
 
+        vm.getAdminAccountInfo = function () {
+            $http.get(API + '/user', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': vm.token
+                }
+            }).then(function (res) {
+                $scope.loadingAccountInfo = false;
+                if (res.status === 200) {
+                    $scope.administrator = res.data.data;
+                }
+            }).catch(function (error) {
+                $scope.loadingAccountInfo = false;
+                errorToasts.evaluateErrors(error.data);
+            });
+        };
+        vm.getAdminAccountInfo();
+
+        $scope.updateAdministratorAccount = function(){
+            $scope.loadingAccountInfo = true;
+            $http.patch(API + '/user', vm.updatedAdministrator ,{
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': vm.token
+                }
+            }).then(function (res) {
+                $scope.loadingAccountInfo = false;
+                if (res.status === 200) {
+                    $scope.administrator = res.data.data;
+                }
+                vm.updatedAdministrator = {};
+            }).catch(function (error) {
+                vm.updatedAdministrator = {};
+                $scope.loadingAccountInfo = false;
+                errorToasts.evaluateErrors(error.data);
+            });
+        };
 
     }
 })();
