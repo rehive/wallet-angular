a7640dfab382a4bf2b695f3e8707ac355f965656
AquiburRKhan <ark.aquib17@gmail.com>
2017-05-04 11:48:17 +0600

    add: added correct api to add currency and is now selectable but cannot yet be added to company's currency

--- src/app/pages/addCurrency/AddCurrencyCtrl.js
+++ src/app/pages/addCurrency/AddCurrencyCtrl.js
@@ -10,8 +10,7 @@
         var vm = this;
         vm.token = cookieManagement.getCookie('TOKEN');
 
-        $scope.changeCurrency = {};
-        $scope.changeCurrency.currencyChoice = {};
+        $scope.addCurrency = {};
         $scope.showConfirmCurrency = false;
         $scope.showCompleteCurrency = false;
         $scope.loadingCurrencies = true;
@@ -32,7 +31,7 @@
             }).then(function (res) {
                 $scope.loadingCurrencies = false;
                 if (res.status === 200) {
-                    $scope.changeCurrency.currencyChoice.code = res.data.data.results[0].code;
+                    $scope.addCurrency.currencyChoice = res.data.data.results[0];
                     $scope.currencyOptions = res.data.data.results;
                 }
             }).catch(function (error) {
@@ -42,6 +41,34 @@
         };
         vm.getCurrencies();
 
+        $scope.addCompanyCurrency = function(){
+
+            var addCurrency = {
+                code: $scope.addCurrency.currencyChoice.code,
+                description: $scope.addCurrency.currencyChoice.description,
+                symbol: $scope.addCurrency.currencyChoice.symbol,
+                unit: $scope.addCurrency.currencyChoice.unit,
+                divisibility: $scope.addCurrency.currencyChoice.divisibility
+            };
+
+            //$scope.loadingCurrencies = true;
+            //$http.post(API + '/admin/currencies/', addCurrency, {
+            //    headers: {
+            //        'Content-Type': 'application/json',
+            //        'Authorization': vm.token
+            //    }
+            //}).then(function (res) {
+            //    $scope.loadingCurrencies = false;
+            //    if (res.status === 200) {
+            //        $scope.addCurrency.currencyChoice.code = res.data.data.results[0].code;
+            //        $scope.currencyOptions = res.data.data.results;
+            //    }
+            //}).catch(function (error) {
+            //    $scope.loadingCurrencies = false;
+            //    errorToasts.evaluateErrors(error.data);
+            //});
+        };
+
         $scope.next = function(){
             $scope.showConfirmCurrency = true;
         };
@@ -49,7 +76,7 @@
         $scope.confirmCurrency = function () {
             $scope.showConfirmCurrency = false;
             $scope.showCompleteCurrency = true;
-        }
+        };
 
     }
 })();
