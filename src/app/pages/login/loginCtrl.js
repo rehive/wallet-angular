0730f527ae2682e08593e036bed1f552c5b24788
AquiburRKhan <ark.aquib17@gmail.com>
2017-05-18 19:46:24 +0600

    change: getting company name from admin/company api when logging in

--- src/app/pages/login/loginCtrl.js
+++ src/app/pages/login/loginCtrl.js
@@ -17,13 +17,11 @@
                 company_id: company_id,
                 password: password
             }).then(function (res) {
-                $rootScope.$pageFinishedLoading = true;
+
                 if (res.status === 200) {
                     cookieManagement.setCookie('TOKEN','Token ' + res.data.data.token);
-                    var companyName = vm.getCompanyName(res.data.data.user.company.split("_"));
-                    cookieManagement.setCookie('COMPANY',companyName);
+                    vm.getCompanyInfo(res.data.data.token);
                     toastr.success('You have successfully logged in with the email address ' + res.data.data.user.email +'!');
-                    $location.path('/dashboard');
                 }
             }).catch(function (error) {
                 console.log(error);
@@ -32,13 +30,22 @@
             });
         };
 
-        vm.getCompanyName = function(companyNameArray){
-            var companyName = '';
-            companyNameArray.forEach(function(word){
-                companyName += stringService.capitalizeWord(word) + ' ';
-            });
-            return companyName;
-        };
-
+        vm.getCompanyInfo = function (token) {
+            if(token) {
+                $http.get(API + '/admin/company/', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Token ' + token
+                    }
+                }).then(function (res) {
+                    $rootScope.$pageFinishedLoading = true;
+                    cookieManagement.setCookie('COMPANY',res.data.data.name);
+                    $location.path('/dashboard');
+                }).catch(function (error) {
+                    $rootScope.$pageFinishedLoading = true;
+                    errorToasts.evaluateErrors(error.data);
+                });
+            }
+        }
     }
 })();
