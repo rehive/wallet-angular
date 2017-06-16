3f1eed27d2a325db9a85cd99bc4f98b7b3fda6ab
AquiburRKhan <ark.aquib17@gmail.com>
2017-06-16 06:09:06 +0600

    add: filter to show decimals in amounts by divisibility

--- src/app/theme/services/currencyConversion.js
+++ src/app/theme/services/currencyConversion.js
@@ -3,7 +3,8 @@
 
     angular.module('BlurAdmin.theme')
         .service('currencyModifiers', currencyModifiers)
-        .filter('currencyModifiersFilter', currencyModifiersFilter);
+        .filter('currencyModifiersFilter', currencyModifiersFilter)
+        .filter('preciseRound',preciseRound);
 
     /** @ngInject */
     function currencyModifiers() {
@@ -26,6 +27,31 @@
     function currencyModifiersFilter(){
         return function (amount,divisibility){
             return  amount / Math.pow(10,divisibility);
+        }
+    }
+
+
+    function preciseRound(){
+        return function (num,decimals){
+            var finalString;
+            var multipliedByDecimalNumber = num*Math.pow(10, decimals);
+            var multipliedByDecimalString = multipliedByDecimalNumber.toString();
+            if(multipliedByDecimalString[0] == '-'){
+                multipliedByDecimalString = multipliedByDecimalString.slice(1);
+                finalString = '-' + insertDot(multipliedByDecimalString,decimals);;
+            } else {
+                finalString = insertDot(multipliedByDecimalString,decimals);
+            }
+
+            function insertDot(multipliedByDecimalString,decimals){
+                var array,indexOfDot;
+                indexOfDot = multipliedByDecimalString.length - decimals;
+                array = multipliedByDecimalString.split('');
+                array.splice(indexOfDot,0,'.');
+                return array.join('');
+            }
+
+            return finalString;
         }
     }
 
