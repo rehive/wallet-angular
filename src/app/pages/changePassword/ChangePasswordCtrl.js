dd39b16437ccdd96db6012e5ebc10ad19f898ad5
AquiburRKhan <ark.aquib17@gmail.com>
2017-07-06 20:41:11 +0600

    add: added change password in settings security

--- src/app/pages/changePassword/ChangePasswordCtrl.js
+++ src/app/pages/changePassword/ChangePasswordCtrl.js
@@ -5,16 +5,39 @@
         .controller('ChangePasswordCtrl', ChangePasswordCtrl);
 
     /** @ngInject */
-    function ChangePasswordCtrl($rootScope,$scope,$http,API,cookieManagement,errorToasts,$location) {
+    function ChangePasswordCtrl($rootScope,$scope,$http,API,cookieManagement,errorToasts,$location,toastr) {
 
         var vm = this;
         vm.token = cookieManagement.getCookie('TOKEN');
+        $scope.passwordChanged = false;
 
         $scope.goToDashboard = function(){
             $rootScope.securityConfigured = true;
             $location.path('/dashboard');
         }
 
+        $scope.changePassword = function (passwordChangeParams) {
+            $scope.changingPassword = true;
+            $http.post(API + '/auth/password/change/', passwordChangeParams, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': vm.token
+                }
+            }).then(function (res) {
+                $scope.changingPassword = false;
+                if (res.status === 200) {
+                  $scope.passwordChanged = true;
+                  toastr.success('You have successfully changed the password!');
+                }
+            }).catch(function (error) {
+                $scope.changingPassword = false;
+                if(error.status == 403){
+                    errorHandler.handle403();
+                    return
+                }
+                errorToasts.evaluateErrors(error.data);
+            });
+        };
 
 
     }
