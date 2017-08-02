1674c66c0e5be36e6f939c469b238ae10e8a5a59
Michail Brynard <mbrynard@gmail.com>
2017-08-02 00:08:59 -0700

    Add server release commands

--- server_tasks.py
+++ server_tasks.py
@@ -11,6 +11,9 @@ import fabfile as fab
 
 import os
 
+from distutils.util import strtobool
+from invoke.exceptions import ParseError
+
 # Utility functions
 
 def format_yaml(template, config):
@@ -72,3 +75,71 @@ def compose(ctx, cmd, config, version_tag):
     """Push image to container registry"""
     execute(fab.set_env, config, version_tag)
     execute(fab.compose, cmd, version_tag)
+
+
+@task
+def next_version(ctx, bump='prerelease'):
+    """
+    Returns incremented version number by looking at git tags
+    """
+    # Get latest git tag:
+    latest_tag = latest_version(ctx)
+
+    if latest_tag:
+        increment = {'prerelease': semver.bump_prerelease,
+                     'patch': semver.bump_patch,
+                     'minor': semver.bump_minor,
+                     'major': semver.bump_major}
+
+        incremented = increment[bump](latest_tag)
+
+    else:
+        incremented = '0.0.1'
+
+    print(incremented)
+
+    return incremented
+
+
+@task
+def latest_version(ctx):
+    """Checks the git tags and returns the current latest version"""
+    ctx.run('git fetch && git fetch --tags')
+    result = ctx.run('git tag --sort=-v:refname', hide='both')
+    latest_tag = result.stdout.split('\n')[0]
+    print(latest_tag)
+    return latest_tag
+
+
+@task
+def docker_release(ctx, config):
+    """Build and push docker images for current release"""
+    config_dict = get_config(config)
+
+    # Create, tag and push docker image:
+    tag = latest_version(ctx)
+
+    image_name = config_dict['IMAGE'].split(':')[0]
+    # build(ctx, config, tag) # included in push for now.
+    push(ctx, config, tag)
+
+    print('Release Info:\n'
+          'Tag: {}\n'
+          'Image: {}\n'.format(tag, image_name))
+
+
+def confirm(prompt='Continue?\n', failure_prompt='User cancelled task'):
+    '''
+    Prompt the user to continue. Repeat on unknown response. Raise
+    ParseError on negative response
+    '''
+    response = input(prompt)
+
+    try:
+        response_bool = strtobool(response)
+    except ValueError:
+        print('Unkown Response. Confirm with y, yes, t, true, on or 1; cancel with n, no, f, false, off or 0.')
+        confirm(prompt, failure_prompt)
+
+    if not response_bool:
+        raise ParseError(failure_prompt)
