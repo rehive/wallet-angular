f26241f431394b8b7fe05d85182ecb319ace4d54
AquiburRKhan <ark.aquib17@gmail.com>
2017-08-11 14:14:15 +0600

    add: added etherum address creation but with bitcoin api

--- src/app/pages/register/ethereumAddress/ethereumAddressCtrl.js
+++ src/app/pages/register/ethereumAddress/ethereumAddressCtrl.js
@@ -5,10 +5,11 @@
         .controller('EthereumAddressCtrl', EthereumAddressCtrl);
 
     /** @ngInject */
-    function EthereumAddressCtrl($rootScope,$scope,$http,cookieManagement,environmentConfig,$location,errorToasts,userVerification) {
+    function EthereumAddressCtrl($rootScope,$scope,$http,cookieManagement,environmentConfig,$location,errorToasts) {
 
         var vm = this;
         vm.token = cookieManagement.getCookie('TOKEN');
+        $scope.ethereum = {address: ''};
         $scope.showAuthNav = false;
         $scope.loadingEtheriumView = true;
 
@@ -22,7 +23,7 @@
                 if (res.status === 200) {
                     $scope.user = res.data.data;
                     $scope.showAuthNav = true;
-                    $scope.loadingEtheriumView = false;
+                    vm.getEthereumAddresses();
                 }
             }).catch(function (error) {
                 $scope.loadingEtheriumView = false;
@@ -31,8 +32,41 @@
         };
         vm.getUserInfo();
 
-        $scope.addEthereumAddress = function(){
-            $location.path('home');
+        vm.getEthereumAddresses = function(){
+            $http.get(environmentConfig.API + '/user/bitcoin-accounts/', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': vm.token
+                }
+            }).then(function (res) {
+                if (res.status === 200) {
+                    if(res.data.data.length > 0){
+                        $location.path('/home')
+                    }
+                    $scope.loadingEtheriumView = false;
+                }
+            }).catch(function (error) {
+                $scope.loadingEtheriumView = false;
+                errorToasts.evaluateErrors(error.data);
+            });
+        };
+
+        $scope.addEthereumAddress = function(address){
+            $http.post(environmentConfig.API + '/user/bitcoin-accounts/',{address: address}, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': vm.token
+                }
+            }).then(function (res) {
+                if (res.status === 201) {
+                    $rootScope.notRegistering = true;
+                    $location.path('home');
+                    $scope.loadingEtheriumView = false;
+                }
+            }).catch(function (error) {
+                $scope.loadingEtheriumView = false;
+                errorToasts.evaluateErrors(error.data);
+            });
         };
 
         $scope.goToNextView = function(){
