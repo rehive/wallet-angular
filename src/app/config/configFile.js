84e8b8e14b275721c177f7c621bd8859f1f80f0f
odinsplasmarifle <generalaetius.jbvb@gmail.com>
2017-08-22 20:27:37 +0200

    Adding ICO API URL

--- src/app/config/configFile.js
+++ src/app/config/configFile.js
@@ -1,2 +1,2 @@
 angular.module("BlurAdmin.config", [])
-.constant("environmentConfig", {"API":"https://staging.rehive.com/api/3", "ICO_API":"https://ico.s.services.rehive.io/api", "COMPANY":"ico_test_1"});
+.constant("environmentConfig", {"API":"https://staging.rehive.com/api/3","COMPANY":"ico_test_1","ICO_API":"https://ico.s.services.rehive.io/api"});
