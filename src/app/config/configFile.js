e3acecec17c0b124d0dad3c27735c810bc8fa1b0
Helghardt Avenant <helghardt@gmail.com>
2017-08-23 07:58:36 -0700

    Adding echarge_demo_1 fro staging.

--- src/app/config/configFile.js
+++ src/app/config/configFile.js
@@ -1,2 +1,2 @@
 angular.module("BlurAdmin.config", [])
-.constant("environmentConfig", {"API":"https://staging.rehive.com/api/3","COMPANY":"ico_test_1","ICO_API":"https://ico.s.services.rehive.io/api","XBT_API":"https://bitcoin.s.services.rehive.com/api/1","ETH_API":"https://ethereum.s.services.rehive.io/api/1"});
+.constant("environmentConfig", {"API":"https://staging.rehive.com/api/3","COMPANY":"echarge_demo_1","ICO_API":"https://ico.s.services.rehive.io/api","XBT_API":"https://bitcoin.s.services.rehive.com/api/1","ETH_API":"https://ethereum.s.services.rehive.io/api/1"});
